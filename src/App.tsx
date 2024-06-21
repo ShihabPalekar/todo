420d0857f9f4172c9ad1c024ec69dcf2bc935cbb
ShihabPalekar <ShihabPalekar23@gmail.com>
2024-06-21 10:19:50 +0530

    add checked input to todo

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,20 @@ const App = () => {
   };
 
   const handleSubmit = () => {
-    const data = { id: `${Date.now()}`, value: todo };
+    const data = { id: `${Date.now()}`, value: todo, checked: false };
     setTodos([...todos, data]);
     setTodo("");
   };
 
-  const removeTodo = (id:any) => {
-    setTodos(todos.filter((i:any) => i.id !== id))
-  }
+  const removeTodo = (id: any) => {
+    setTodos(todos.filter((i: any) => i.id !== id));
+  };
 
-  console.log(todos)
+  const handleCheck = (id: any) => {
+    setTodos(
+      todos.map((i: any) => (i.id === id ? { ...i, checked: !i.checked } : i))
+    );
+  };
 
   return (
     <div className="App">
@@ -32,28 +36,33 @@ const App = () => {
       </div>
 
       <div>
-        <ul>
-          {todos.map((i: any) => (
-            <li key={i.id}>
-              <span>{i.value}</span>
-              <span onClick={() => removeTodo(i.id)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="size-6"
-                  style={{width: "16px", height: "16px"}}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-            </li>
-          ))}
-        </ul>
+        {todos.map((i: any) => (
+          <div key={i.id}>
+            <span>
+              <input
+                checked={i.checked}
+                onChange={() => handleCheck(i.id)}
+                type="checkbox"
+              />
+            </span>
+            <span>{i.value}</span>
+            <span onClick={() => removeTodo(i.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+                style={{ width: "16px", height: "16px" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
