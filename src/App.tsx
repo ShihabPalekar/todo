2af47d8f72189633353c08ddbd84b44f098ad36b
ShihabPalekar <ShihabPalekar23@gmail.com>
2024-06-21 09:02:52 +0530

    add new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState<any>([]);
+
+  const handleChange = (e: any) => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const data = { id: todos.length + 1, value: todo };
+    setTodos([...todos, data]);
+    setTodo("");
+  };
+
   return (
     <div className="App">
-      App
+      <div>
+        <div>
+          <input value={todo} onChange={handleChange} />
+        </div>
+        <div>
+          <button onClick={handleSubmit}>Add Todo</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
